08d14b262ac094f0687ee34182a8bfedb2012d89
youssefehab25 <123908594+youssefehab25@users.noreply.github.com>
2023-12-25 14:11:24 +0200

    change getx with OBX

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -7,51 +7,51 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    HomePageController controller = Get.put(HomePageController());
     return Scaffold(
       appBar: AppBar(
         title: const Center(child: Text("COUNTER")),
         backgroundColor: Colors.orange,
       ),
-      body:GetX<HomePageController>(
-        init: HomePageController(),
-        builder: (controller) =>Align(
-          child: Container(
-            height: 150,
-            width: 300,
-            alignment: Alignment.center,
-            decoration:const BoxDecoration(
-             borderRadius: BorderRadius.all(Radius.circular(30)),
-              color: Colors.orange,
-              
-
-              
-            ),
-            child: Column(mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row( 
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(onPressed: (){
-                  controller.increment();
-          
-                }, icon:const Icon(Icons.add) ),
-                const SizedBox(width: 5,),
-                 Text("${controller.counter.value}"),
-                const SizedBox(width: 5,),
-                IconButton(onPressed: (){              controller.decrement();
-          
-          
-                }, icon:const Icon(Icons.remove) ),
-          
-              ]
-          
+      body:Obx(()=>
+        Align(
+            child: Container(
+              height: 150,
+              width: 300,
+              alignment: Alignment.center,
+              decoration:const BoxDecoration(
+               borderRadius: BorderRadius.all(Radius.circular(30)),
+                color: Colors.orange,
+                
+      
+                
               ),
-            ],
+              child: Column(mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row( 
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(onPressed: (){
+                    controller.increment();
+            
+                  }, icon:const Icon(Icons.add) ),
+                  const SizedBox(width: 5,),
+                   Text("${controller.counter.value}"),
+                  const SizedBox(width: 5,),
+                  IconButton(onPressed: (){              controller.decrement();
+            
+            
+                  }, icon:const Icon(Icons.remove) ),
+            
+                ]
+            
                 ),
+              ],
+                  ),
+            ),
           ),
-        ),
-       )
-
-    );
+      ),
+       );
   }
 }
